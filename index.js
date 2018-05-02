88fe9d527eb08385597a74bae88669b99bc2f523
TUtso <utsombstu@gmail.com>
2018-05-02 22:25:20 +0600

    redirecting to google authentication

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
- const app = express()
+const app = express()
+const keys = require('./config/keys.js')
 
+ passport.use(new GoogleStrategy({
+     clientID : keys.googleClientID,
+     clientSecret : keys.googleClientSecret,
+     callbackURL : 'auth/google/callback'
+ },  
+  accessToken => {
+      console.log(accessToken);
+  }  
+))
 
- passport.use(new GoogleStrategy())
+app.get('/auth/google', passport.authenticate('google',{
+    scope: ['profile','email']
+}))
  
 
  const PORT = process.env.PORT || 5000
