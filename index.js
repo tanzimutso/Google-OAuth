ea63f0af13e17563142cc9a27ee9927154162396
TUtso <utsombstu@gmail.com>
2018-05-02 21:58:37 +0600

    adding client id and client secret

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
  const app = express()
 
- //passport.use(new GoogleStrategy())
+
+ passport.use(new GoogleStrategy())
  
 
  const PORT = process.env.PORT || 5000
