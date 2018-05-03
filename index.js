58b89b3c63eb0b0444211e33464612d44574a685
TUtso <utsombstu@gmail.com>
2018-05-04 01:15:40 +0600

    access token, refresh token and profile fetch from google API

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const keys = require('./config/keys.js')
  passport.use(new GoogleStrategy({
      clientID : keys.googleClientID,
      clientSecret : keys.googleClientSecret,
-     callbackURL : 'auth/google/callback'
+     callbackURL : '/auth/google/callback'
  },  
-  accessToken => {
-      console.log(accessToken);
+  (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken: ', accessToken);
+      console.log('refreshToken: ', refreshToken);
+      console.log('profile: ', profile);
   }  
 ))
 
 app.get('/auth/google', passport.authenticate('google',{
     scope: ['profile','email']
 }))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
  
 app.get('/', (req,res) => {
     res.send('Welcome')
