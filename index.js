e8e8392157391f48746f31e338fe1f80b06640b3
TUtso <utsombstu@gmail.com>
2018-05-03 23:37:44 +0600

    root route modified

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get('/auth/google', passport.authenticate('google',{
     scope: ['profile','email']
 }))
  
+app.get('/', (req,res) => {
+    res.send('Welcome')
+})
 
  const PORT = process.env.PORT || 5000
  app.listen(PORT)
